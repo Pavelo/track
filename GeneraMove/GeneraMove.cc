dd9bd144fa4148677712feb927f7cddb0bd830b5
Pavelo <craitnag@gmail.com>
2009-06-24 14:15:58 +0200

    basta invertire if

--- GeneraMove/GeneraMove.cc
+++ GeneraMove/GeneraMove.cc
@@ -238,7 +238,7 @@ void GeneraMove::GetCamera(const ONotifyEvent& event) {
 	{
 		for (y=0; y < grids_y; y++)
 		{
-		  if(x == 3 && (y == 1 || y == 2)){
+		  if(x == 2   & (y == 1 || y == 2)){
 		  	if (pix_count[x][y] > 410)
 				grid_matrix[x][y] = 0;
 			else
@@ -253,7 +253,7 @@ void GeneraMove::GetCamera(const ONotifyEvent& event) {
 		}
 	}
 
-	OSYSDEBUG(("pix count davanti: %d\n", pix_count[3][1]));
+	//OSYSDEBUG(("pix count: %d  %d  %d  %d\n", pix_count[3][1],pix_count[3][2],pix_count[2][1],pix_count[2][2]));
 	//minefield
 	//int max_x = sizeof(grid_matrix[0]) / sizeof(int);
 	//int max_y = sizeof(grid_matrix) /sizeof(int);
@@ -284,145 +284,20 @@ void GeneraMove::GetCamera(const ONotifyEvent& event) {
 		}
 	}
 
-	OSYSDEBUG(("grid matrix davanti: %d\n", grid_matrix[3][1]));
 
+	OSYSDEBUG(("grid matrix: %d  %d  %d  %d\n %d  %d  %d  %d \n\n", 
+	grid_matrix[2][0],grid_matrix[2][1],grid_matrix[2][2],grid_matrix[2][3],
+	grid_matrix[3][0],grid_matrix[3][1],grid_matrix[3][2],grid_matrix[3][3]));
+	
+	OSYSDEBUG(("lum:\n %d  %d  %d  %d\n %d  %d  %d  %d \n", 
+	y_count[2][0],y_count[2][1],y_count[2][2],y_count[2][3],
+	y_count[3][0],y_count[3][1],y_count[3][2],y_count[3][3]));
 
 	Motion::MotionCommand command;
 	memset(&command, 0, sizeof(command));
 
 
-
-	// if (grid_matrix[3][1] < 3 && grid_matrix[2][1] < 3 && grid_matrix[1][1] < 3){
-	//   OSYSDEBUG(("vado dritto di 3 sx\n"));
-	  // command.motion_cmd=Motion::MOTION_WALK_TROT;
-	  // command.head_cmd=Motion::HEAD_LOOKAT;
-	  // command.tail_cmd=Motion::TAIL_NO_CMD;
-	  // command.head_lookat=vector3d(150,0,50);
-	  // command.vx=100;
-	  // command.vy=0;
-	  // command.va=0.05;
-	  // if (sph ==1){
-	  //   subject[sbjMotionControl]->SetData(&command,sizeof(Motion::MotionCommand));
-	  //   subject[sbjMotionControl]->NotifyObservers();
-	  //   sph=0;
-	  // }
-	//   Wait(static_cast<longword>(2000000000));
-	// }
-	// else if (grid_matrix[3][2] < 3 && grid_matrix[2][2] < 3 && grid_matrix[1][2] < 3)
-	//   {
-	//     OSYSDEBUG(("vado dritto di 3 dx\n"));
-	    // command.motion_cmd=Motion::MOTION_WALK_TROT;
-	    // command.head_cmd=Motion::HEAD_LOOKAT;
-	    // command.tail_cmd=Motion::TAIL_NO_CMD;
-	    // command.head_lookat=vector3d(150,0,50);
-	    // command.vx=100;
-	    // command.vy=0;
-	    // command.va=0.05;
-	    // if (sph ==1){
-	    //   subject[sbjMotionControl]->SetData(&command,sizeof(Motion::MotionCommand));
-	    //   subject[sbjMotionControl]->NotifyObservers();
-	    //   sph=0;
-	    // }
-	   //  Wait(static_cast<longword>(2000000000));
-	//   }
-	// else if (grid_matrix[3][1] < 3 && grid_matrix[2][1] < 3 && grid_matrix[1][0] < 3)
-	//   {
-	//     OSYSDEBUG(("giro a sinistra di 3\n"));
-	//     Wait(static_cast<longword>(2000000000));
-	//   } 
-	// else if (grid_matrix[3][2] < 3 && grid_matrix[2][2] < 3 && grid_matrix[1][3] < 3 )
-	//   {
-	//     OSYSDEBUG(("giro a destra di 3\n"));
-	//     Wait(static_cast<longword>(2000000000));
-	//   }
-	// if ((grid_matrix[3][1] < 3 && grid_matrix[2][1] < 3) || (grid_matrix[3][2] < 3 && grid_matrix[2][2] < 3))
-	//   {
-	//     OSYSDEBUG(("vado dritto di 2\n"));
-	//     Wait(static_cast<longword>(2000000000));
-	//   }
-	// else if (grid_matrix[3][1] < 3 && grid_matrix[3][0] < 3)
-	//   {
-	//     OSYSDEBUG(("giro a sinistra di 2\n"));
-	    // command.motion_cmd=Motion::MOTION_WALK_TROT;
-	    // command.head_cmd=Motion::HEAD_LOOKAT;
-	    // command.tail_cmd=Motion::TAIL_NO_CMD;
-	    // command.head_lookat=vector3d(150,0,50);
-	    // command.vx=100;
-	    // command.vy=30;
-	    // command.va=0.05;
-	    // if (sph ==1){
-	    //   subject[sbjMotionControl]->SetData(&command,sizeof(Motion::MotionCommand));
-	    //   subject[sbjMotionControl]->NotifyObservers();
-	    //   sph=0;
-	    // }
-	  //   Wait(static_cast<longword>(1000000000));
-
-	//   }
-	// else if (grid_matrix[3][2] < 3 && grid_matrix[3][3] < 3)
-	//   {
-	//     OSYSDEBUG(("giro a destra di 2\n"));
-	    // command.motion_cmd=Motion::MOTION_WALK_TROT;
-	    // command.head_cmd=Motion::HEAD_LOOKAT;
-	    // command.tail_cmd=Motion::TAIL_NO_CMD;
-	    // command.head_lookat=vector3d(150,0,50);
-	    // command.vx=100;
-	    // command.vy=0;
-	    // command.va=-0.35;
-	    // if (sph ==1){
-	    //   subject[sbjMotionControl]->SetData(&command,sizeof(Motion::MotionCommand));
-	    //   subject[sbjMotionControl]->NotifyObservers();
-	    //   sph=0;
-	    // }
-	  //   Wait(static_cast<longword>(500000000));
-	  // }
-	// else if (grid_matrix[3][1] < 3)
-	//   {
-	//     OSYSDEBUG(("vado dritto di 1\n"));
-	//     Wait(static_cast<longword>(1000000000));
-	//   }
-	// else if (grid_matrix[3][2] < 3)
-	//   {
-	//     OSYSDEBUG(("vado dritto di 1\n"));
-	//     Wait(static_cast<longword>(1000000000));
-	//   }
-	// else if (grid_matrix[3][1] < 3) 
-	//   {
-	//     OSYSDEBUG(("giro a sinistra di 1\n"));
-	//     Wait(static_cast<longword>(1000000000));
-	//   }
-	// else if (grid_matrix[3][2] < 3)
-	//   {
-	//     OSYSDEBUG(("giro a destra di 1\n"));
-	//     // command.motion_cmd=Motion::MOTION_WALK_TROT;
-	//     // command.head_cmd=Motion::HEAD_LOOKAT;
-	//     // command.tail_cmd=Motion::TAIL_NO_CMD;
-	//     // command.head_lookat=vector3d(150,0,50);
-	//     // command.vx=0;
-	//     // command.vy=0;
-	//     // command.va=0.85;
-	//     // if (sph ==1){
-	//     //   subject[sbjMotionControl]->SetData(&command,sizeof(Motion::MotionCommand));
-	//     //   subject[sbjMotionControl]->NotifyObservers();
-	//     //   sph=0;
-	//     // }
-	//     Wait(static_cast<longword>(1500000000));
-	//   }
-	// else{
-	//     command.motion_cmd=Motion::MOTION_STAND_NEUTRAL;
-	//     command.head_cmd=Motion::HEAD_LOOKAT;
-	//     command.tail_cmd=Motion::TAIL_NO_CMD;
-	//     command.head_lookat=vector3d(150,0,50);
-	//     command.vx=0;
-	//     command.vy=0;
-	//     command.va=0.85;
-	//     if (sph ==1){
-	//       subject[sbjMotionControl]->SetData(&command,sizeof(Motion::MotionCommand));
-	//       subject[sbjMotionControl]->NotifyObservers();
-	//       sph=0;
-	//     }
-	// }
-
-	if(grid_matrix[3][1] < 3 || grid_matrix[3][2] < 3 || grid_matrix[3][0] < 3 || grid_matrix[3][3] < 3){
+	if(grid_matrix[3][1] < 3 && grid_matrix[3][2] < 3 && grid_matrix[2][1] < 3){
 	    OSYSDEBUG(("dritto\n"));
 	    command.motion_cmd=Motion::MOTION_WALK_TROT;
 	    command.head_cmd=Motion::HEAD_LOOKAT;
@@ -436,9 +311,9 @@ void GeneraMove::GetCamera(const ONotifyEvent& event) {
 	      subject[sbjMotionControl]->NotifyObservers();
 	      sph=0;
 	    }
-	    Wait(static_cast<longword>(1000000000));
+	    Wait(static_cast<longword>(500000000));
 	}
-	else if ((y_count[3][0] < 1000 )|| (y_count[2][0] < 1000 )){
+	/*else if ((y_count[3][0] > 1000 )|| (y_count[2][0] > 1000 )){
     OSYSDEBUG(("sinistra\n"));
 	    command.motion_cmd=Motion::MOTION_WALK_TROT;
 	    command.head_cmd=Motion::HEAD_LOOKAT;
@@ -452,11 +327,11 @@ void GeneraMove::GetCamera(const ONotifyEvent& event) {
 	      subject[sbjMotionControl]->NotifyObservers();
 	      sph=0;
 	    }
-	    Wait(static_cast<longword>(2000000000));
-	}
+	    Wait(static_cast<longword>(200000000));
+	}*/
 	
 	
-	else if ((y_count[2][1] < 1000 )|| (y_count[2][2] < 1000 )){
+	else if ((y_count[3][1] > 1000 ) && y_count[3][2] > 1000  && y_count[2][1] > 1000){
 	    OSYSDEBUG(("sinistra\n"));
 	    command.motion_cmd=Motion::MOTION_WALK_TROT;
 	    command.head_cmd=Motion::HEAD_LOOKAT;
@@ -470,7 +345,7 @@ void GeneraMove::GetCamera(const ONotifyEvent& event) {
 	      subject[sbjMotionControl]->NotifyObservers();
 	      sph=0;
 	    }
-	    Wait(static_cast<longword>(2000000000));
+	    Wait(static_cast<longword>(1000000000));
 	}
 	else {
 	    OSYSDEBUG(("destra\n"));
@@ -486,10 +361,10 @@ void GeneraMove::GetCamera(const ONotifyEvent& event) {
 	      subject[sbjMotionControl]->NotifyObservers();
 	      sph=0;
 	    }
-	    Wait(static_cast<longword>(2000000000));
+	    Wait(static_cast<longword>(1500000000));
 	}	
 
-OSYSDEBUG(("lum davanti: %d\n", y_count[3][1]));
+//OSYSDEBUG(("lum davanti: %d\n", y_count[3][1]));
 	observer[event.ObsIndex()]->AssertReady();
 }
 
